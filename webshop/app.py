32ac1c3a99d2673903344ce21c9620e29490ae4c
Johannes Sjölund <j.sjolund@gmail.com>
2015-11-12 00:38:05 +0100

    Show assets, generate example tables

--- webshop/app.py
+++ webshop/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, g, render_template, request
-import MySQLdb
+import MySQLdb as mdb
 
 app = Flask(__name__)
 
@@ -10,94 +10,119 @@ USERNAME = 'little-webshop'
 PASSWORD = 'little-webshop-password-123'
 DATABASE = 'little_webshop'
 
-def init_sqlite():
-    """ Create a new database or clear the old one """
-    g.db = get_db()
-    with app.open_resource('schema-sqlite.sql', mode='r') as f:
-        g.db.cursor().executescript(f.read())
-    g.db.commit()
 
-def init_mysql():
-    """ Create a new database or clear the old one """
-    g.db = get_db()
-    with app.open_resource('schema-mysql.sql', mode='r') as f:
-        for line in f:
-            g.db.execute(line)
-    g.db.commit()
-
-def get_db():
-    return MySQLdb.connect(host=HOST, port=2222, user=USERNAME, passwd=PASSWORD, db=DATABASE).cursor()
+def connect_db():
+    return mdb.connect(host=HOST, port=2222, user=USERNAME, passwd=PASSWORD, db=DATABASE)
 
 
 @app.before_request
 def before_request():
     """ This is run when any page is requested """
     try:
-        g.db = get_db()
+        g.db = connect_db()
     except Exception as e:
-        print(e)
+        print e
 
 
 @app.teardown_request
 def teardown_request(exception):
     """ This run when the requested page has been served """
-    g.db = get_db()
-    if g.db is not None:
-        g.db.close()
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
     else:
-        print("DB is null")
-
-
-@app.route("/")
-def home():
-    """  """
-    g.db = get_db()
-    g.db.execute('select login, password from Customer order by idCustomer desc')
-    entries = g.db.fetchall()
-    return render_template("home.html", entries=entries)
-
-
-@app.route('/', methods=['POST'])
-def home_post():
-    """  """
-    if "button-save" in request.form:
-        g.db = get_db()
-        g.db.execute('insert into Customer (login, password) values (%s, %s)',
-                     [request.form['text-login'], request.form['text-password']])
-        g.db.connection.commit()
-    elif "button-clear" in request.form:
-        g.db = get_db()
-        g.db.execute('delete from Customer')
-        g.db.connection.commit()
-    return home()
+        print "DB was null!"
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+
+@app.route("/")
+def home():
+    db = getattr(g, 'db', None).cursor(mdb.cursors.DictCursor)
+    db.execute('select * from AssetCategory order by idCategory desc')
+    category_rows = db.fetchall()
+    categorized_items = []
+    for category_row in category_rows:
+        db.execute('select * from Asset where AssetCategory_idCategory=(%s)', [category_row["idCategory"]])
+        assets_in_category = db.fetchall()
+        categorized_items.append( (category_row["categoryName"], assets_in_category) )
+    return render_template('home.html', category_rows=category_rows, categorized_items=categorized_items)
+
+
+@app.route('/', methods=['POST'])
+def home_post():
+    db = getattr(g, 'db', None).cursor(mdb.cursors.DictCursor)
+    if "button-save-category" in request.form:
+        db.execute('insert into AssetCategory (categoryName) values (%s)',
+                   [request.form['text-categoryName']])
+        db.connection.commit()
+    if "button-save-asset" in request.form:
+        db.execute('select idCategory from AssetCategory where categoryName=(%s)',
+                   [request.form['select-category']])
+        idCategory = db.fetchall()[0]['idCategory']
+        db.execute( 'insert into Asset '
+                    '(name, price, amountInStore, imagePath, AssetCategory_idCategory)'
+                    ' values '
+                    '(%s, %s, %s, %s, %s)',
+                   [
+                       request.form['text-name'],
+                       request.form['text-price'],
+                       request.form['text-amountInStore'],
+                       request.form['text-imagePath'],
+                       idCategory
+                   ])
+        db.connection.commit()
+    elif "button-clear-asset" in request.form:
+        db.execute('delete from Asset')
+    elif "button-clear-category" in request.form:
+        db.execute('delete from Category')
+    db.connection.commit()
+    return home()
+
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
 
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    db = getattr(g, 'db', None).cursor(mdb.cursors.DictCursor)
+    db.execute('select * from Customer order by idCustomer desc')
+    rows = db.fetchall()
+    return render_template('signup.html', rows=rows)
+
 
 @app.route('/signup', methods=['POST'])
 def signup_post():
-    """  """
+    db = getattr(g, 'db', None).cursor(mdb.cursors.DictCursor)
+    print("signup")
+    statement_insert = 'insert into Customer ' \
+                       '(login, password, firstName, lastName, streetAddress, postCode, postTown, phoneNr, email)' \
+                       ' values ' \
+                       '(%s, %s, %s, %s, %s, %s, %s, %s, %s)'
     if "button-save" in request.form:
-        g.db = get_db()
-        g.db.execute('insert into Customer (login, password) values (%s, %s)',
-                     [request.form['text-login'], request.form['text-password']])
-        g.db.connection.commit()
+        db.execute(statement_insert,
+                   [
+                       request.form['text-login'],
+                       request.form['text-password'],
+                       request.form['text-firstName'],
+                       request.form['text-lastName'],
+                       request.form['text-streetAddress'],
+                       request.form['text-postCode'],
+                       request.form['text-postTown'],
+                       request.form['text-phoneNr'],
+                       request.form['text-email']
+                   ])
+        db.connection.commit()
     elif "button-clear" in request.form:
-        g.db = get_db()
-        g.db.execute('delete from Customer')
-        g.db.connection.commit()
+        db.execute('delete from Customer')
+        db.connection.commit()
     return signup()
+
 
 if __name__ == '__main__':
     app.secret_key = SECRET_KEY
---
+++ webshop/testdata-mysql.sql
@@ -0,0 +1,67 @@
+
+INSERT INTO AssetCategory (categoryName) VALUES ('fish');
+INSERT INTO AssetCategory (categoryName) VALUES ('bread');
+INSERT INTO AssetCategory (categoryName) VALUES ('vegetables');
+
+INSERT INTO Asset (name, price, amountInStore, imagePath, AssetCategory_idCategory)
+VALUES ('Pike', 39, 23, 'http://loddea.hosterspace.com/wp-content/uploads/2011/09/Gädda.jpg',
+(SELECT idCategory from AssetCategory WHERE categoryName='fish'));
+
+INSERT INTO Asset (name, price, amountInStore, imagePath, AssetCategory_idCategory)
+VALUES ('Dace', 29, 56, 'http://www.finnake.se/fiskar/fish/mort.gif',
+(SELECT idCategory from AssetCategory WHERE categoryName='fish'));
+
+INSERT INTO Asset (name, price, amountInStore, imagePath, AssetCategory_idCategory)
+VALUES ('Perch', 12, 186, 'http://www.finnake.se/fiskar/fish/abborre.gif',
+(SELECT idCategory from AssetCategory WHERE categoryName='fish'));
+
+
+
+INSERT INTO Asset (name, price, amountInStore, imagePath, AssetCategory_idCategory)
+VALUES ('Light bread', 42, 6, 'http://www.jonasbergqvist.se/upl/images/267022.jpg',
+(SELECT idCategory from AssetCategory WHERE categoryName='bread'));
+
+INSERT INTO Asset (name, price, amountInStore, imagePath, AssetCategory_idCategory)
+VALUES ('Dark bread', 39, 68, 'http://www.ninasmatrecept.se/bilder/2009/10/gojibars-brod.jpg',
+(SELECT idCategory from AssetCategory WHERE categoryName='bread'));
+
+INSERT INTO Asset (name, price, amountInStore, imagePath, AssetCategory_idCategory)
+VALUES ('Loaf', 29, 3, 'http://cdn2.cdnme.se/cdn/9-2/944009/images/2009/limpa_1169038350_27562149.jpg',
+(SELECT idCategory from AssetCategory WHERE categoryName='bread'));
+
+
+
+INSERT INTO Asset (name, price, amountInStore, imagePath, AssetCategory_idCategory)
+VALUES ('Tomato', 19, 388, 'http://www.felix.se/wp-content/uploads/2010/11/tomat1.png',
+(SELECT idCategory from AssetCategory WHERE categoryName='vegetables'));
+
+INSERT INTO Asset (name, price, amountInStore, imagePath, AssetCategory_idCategory)
+VALUES ('Carrot', 13, 88, 'http://www.martastradgard.se/morot.JPG',
+(SELECT idCategory from AssetCategory WHERE categoryName='vegetables'));
+
+INSERT INTO Asset (name, price, amountInStore, imagePath, AssetCategory_idCategory)
+VALUES ('Cucumber', 32, 128, 'https://static.mathem.se/shared/images/products/large/gurka-klass1-1.jpg',
+(SELECT idCategory from AssetCategory WHERE categoryName='vegetables'));
+
+INSERT INTO Asset (name, price, amountInStore, imagePath, AssetCategory_idCategory)
+VALUES ('Lettuce', 5, 32, 'http://ppl.nu/wp-content/isbergssallad.jpg',
+(SELECT idCategory from AssetCategory WHERE categoryName='vegetables'));
+
+INSERT INTO Asset (name, price, amountInStore, imagePath, AssetCategory_idCategory)
+VALUES ('Avocado', 15, 32, 'http://bodyandplate.se/wp-content/uploads/2015/01/avokado-namaz.jpg',
+(SELECT idCategory from AssetCategory WHERE categoryName='vegetables'));
+
+INSERT INTO Asset (name, price, amountInStore, imagePath, AssetCategory_idCategory)
+VALUES ('Cauliflower', 5, 111, 'http://www.portofruit.com/img/big/cauliflowers.jpg',
+(SELECT idCategory from AssetCategory WHERE categoryName='vegetables'));
+
+
+
+INSERT INTO Customer (login, password, firstName, lastName, streetAddress, postCode, postTown, phoneNr, email)
+VALUES ('chefen', 'password1', 'Henrik', 'Hansson', 'Balderstensgatan', 2342, 'Falköping', 0766034933234, 'chefen@mincoolaaddress.com');
+
+INSERT INTO Customer (login, password, firstName, lastName, streetAddress, postCode, postTown, phoneNr, email)
+VALUES ('testperson1', 'password2', 'Kalle', 'Olofsson', 'Margaretagatan', 123432, 'Falköping', 2766034933214, 'test@asdfqwersadf.com');
+
+INSERT INTO Customer (login, password, firstName, lastName, streetAddress, postCode, postTown, phoneNr, email)
+VALUES ('testperson2', 'password3', 'Olle', 'Karlsson', 'Westerbergsgatan', 32, 'Falköping', 7766034933234, 'test@rtutyr.com');
