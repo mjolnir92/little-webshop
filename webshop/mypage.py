b0efac81bb2fae1f4710c8c78b79a87ef145c27d
mjolnir92 <xerzeon@gmail.com>
2015-11-27 00:05:45 +0100

    Categories should now show while in 'mypage'

--- webshop/mypage.py
+++ webshop/mypage.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template, g, request
 import MySQLdb as mdb
 from flask.ext.login import current_user
+from db_utils import get_all_categories
 
 mypage_page = Blueprint('mypage_page', __name__, template_folder='templates')
 
 @mypage_page.route('/mypage')
 def mypage():
-	return render_template('mypage.html')
+	return render_template('mypage.html', all_category_rows=get_all_categories(db))
