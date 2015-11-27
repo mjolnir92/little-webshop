ca63c913bb42765f04dde2f659cecfbda6c6838b
mjolnir92 <xerzeon@gmail.com>
2015-11-27 02:01:02 +0100

    Attempt to make it possible to edit user information, most likely broken...

--- webshop/mypage.py
+++ webshop/mypage.py
@@ -6,6 +6,33 @@ from db_utils import get_all_categories
 mypage_page = Blueprint('mypage_page', __name__, template_folder='templates')
 
 @mypage_page.route('/mypage')
-def mypage():
+def mypage(message=None):
 	db = getattr(g, 'db', None).cursor(mdb.cursors.DictCursor)
-	return render_template('mypage.html', all_category_rows=get_all_categories(db))
+	db.execute('select * from User order by idUser desc')
+	rows = db.fetchall()
+	return render_template('mypage.html', all_category_rows=get_all_categories(db), rows=rows, message=message)
+
+@mypage_page.route('/mypage', methods=['POST'])
+def edit_post():
+	db = getattr(g, 'db', None).cursor(mdb.cursors.DictCursor)
+
+	statement_insert = 'update User set ' \
+                       '(login, password, firstName, lastName, streetAddress, postCode, postTown, phoneNr, email)' \
+                       ' values ' \
+                       '(%s, %s, %s, %s, %s, %s, %s, %s, %s)'
+    try:
+        db.execute(statement_insert,
+                   [
+                       request.form['text-login'],
+                       request.form['text-password'],
+                       request.form['text-firstName'],
+                       request.form['text-lastName'],
+                       request.form['text-streetAddress'],
+                       request.form['text-postCode'],
+                       request.form['text-postTown'],
+                       request.form['text-phoneNr'],
+                       request.form['text-email']
+                   ])
+        db.connection.commit()
+	except mdb.IntegrityError:
+        return signup(message='User name is taken!')
